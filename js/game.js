9957d04056b1b064328c6da7bc66b59a86d2b852
Isaac Razabi <82380425+IsaacRazabi@users.noreply.github.com>
2021-05-19 10:49:30 +0300

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,26 +1,37 @@
 "use strict";
 
 var gBoard;
+var gLevel = {
+  SIZE: 4,
+  MINES: 2,
+};
+var gGame = { isOn: false, shownCount: 0, markedCount: 0, secsPassed: 0 };
+const MINE = "💣";
 
 function init() {
-    gBoard =  buildBoard();
-    renderBoard(gBoard)
+  gBoard = buildBoard();
+  renderBoard(gBoard);
 }
 
 // bealding the board and create every cell
 function buildBoard() {
-  var board = createMat(4);
+  var board = createMat(gLevel.SIZE);
   for (var i = 0; i < board.length; i++) {
     for (var j = 0; j < board.length; j++) {
       var cell = {
-        type: null,
-        gameElement: null,
+        minesAroundCount: 4,
+        isShown: true,
         isMine: false,
-        isEmpty: true,
+        isMarked: true,
       };
       board[i][j] = cell;
+      if ((i===0 && j ===1) || (i===2 && j===3)) {
+        board[i][j] = MINE
+      }
     }
   }
+
+
   return board;
 }
 
@@ -29,24 +40,22 @@ function renderBoard(board) {
   for (var i = 0; i < board.length; i++) {
     strHTML += "<tr>\n";
     for (var j = 0; j < board.length; j++) {
-    //   var currCell = board[i][j];
-
-      // var cellClass = getClassName({ i: i, j: j });
-
-      // cellClass += currCell.type === FLOOR ? " floor" : " wall";
-    //   class="cell ${cellClass}"
-
-      strHTML += `\t<td> "S " \n`;
+      //   var currCell = board[i][j];
+      var cellClass = getClassName({ i: i, j: j }); // returne string of form : 'cell-0-0'
+      cellClass += " floor ";
+      if (i===0 && j ===1) cellClass+= 'mine';
+      if(i===2 && j===3) cellClass+= 'mine';
+      //  do condtion og the class each cell get
+      strHTML += `\t<td  class="cell ${cellClass}" onclick="moveTo(${i},${j})" >\n`;
       strHTML += `\t</td>\n`;
     }
-    strHTML += "</tr>\n";
-    console.log(strHTML);
+    strHTML += `</tr>\n`;
   }
-
   var elBoard = document.querySelector(".board");
   elBoard.innerHTML = strHTML;
 }
 
+// create an empty matrix
 function createMat(length) {
   var mat = [];
   for (var i = 0; i < length; i++) {
@@ -56,4 +65,22 @@ function createMat(length) {
     }
   }
   return mat;
+}
+
+function getClassName(location) {
+  var cellClass = "cell-" + location.i + "-" + location.j;
+  return cellClass;
+}
+
+function setMinesNegsCount(board) {}
+
+function getEmptyCells() {
+  var empty = [];
+  for (var i = 1; i < gBoard.length - 1; i++) {
+    for (var j = 1; j < gBoard[0].length - 1; j++) {
+      var currCell = gBoard[i][j];
+      if (!currCell.gameElement) empty.push({ i, j });
+    }
+  }
+  return empty;
 }
