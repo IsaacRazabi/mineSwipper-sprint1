b48f88d34db810129889ce56a4ee8057066eace9
Isaac Razabi <82380425+IsaacRazabi@users.noreply.github.com>
2021-05-20 20:26:54 +0300

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -8,7 +8,6 @@ var gLevel = {
   SIZE: 4,
   MINES: 2,
 };
-
 var gCount = gLevel.SIZE;
 var gGame = {
   isOn: false,
@@ -22,46 +21,26 @@ var gIsHitMine = false;
 var gIsFlag = false;
 var gLife = 3;
 var gLastPos = [];
-init();
 
 function init() {
   gBoard = buildBoard();
   document.querySelector(".win").innerText = "🙂";
-  document.querySelector(".gameStat").innerText='';
-//   var time = 0;
+  document.querySelector(".gameStat").innerText = "";
   gIsFirstMove = true;
   gGame.markedCount = 0;
   gGame.shownCount = 0;
   gTrueFlages = 0;
   gLife = 3;
-
-//   startTimer()
-//   gInterval = setInterval(function () {
-//     time += 1;
-//     var elTimePassed = document.querySelector(".timePassed");
-//     elTimePassed.innerText = `${time}`;
-//   }, 1000);
   renderBoard(gBoard);
 }
 
-// create an empty matrix
-function createMat(length) {
-  var mat = [];
-  for (var i = 0; i < length; i++) {
-    mat[i] = [];
-    for (var j = 0; j < length; j++) {
-      mat[i][j] = "";
-    }
-  }
-  return mat;
-}
 // bealding the MODAL  - KEEPING DATA
 function buildBoard() {
   var board = createMat(gLevel.SIZE);
   for (var i = 0; i < board.length; i++) {
     for (var j = 0; j < board.length; j++) {
       var cell = {
-        minesAroundCount: 4,
+        // minesAroundCount: 4,
         isShown: true,
         isMine: false,
         isMarked: true,
@@ -70,28 +49,18 @@ function buildBoard() {
       board[i][j] = cell;
     }
   }
-  // changing .ismine change all the mat !
-
   return board;
 }
 
-// BULDING THE DISPLAY OF THE PAGE BY HTML
+// BULDING THE DISPLAY OF THE PAGE BY rending to DOM
 function renderBoard(board) {
   var strHTML = "";
   for (var i = 0; i < board.length; i++) {
     strHTML += "<tr>\n";
     for (var j = 0; j < board.length; j++) {
-      //   var currCell = board[i][j];
       var cellClass = getClassName({ i: i, j: j }); // returne string of form : 'cell-0-0'
       cellClass += " floor ";
-      //gice them class of mine
-      //   if (board[i][j].isMine) {
-      //     cellClass += "mine";
-      //   }
       strHTML += `\t<td  class="cell ${cellClass}" onclick="cellClicked(${i},${j})" >\n`;
-      //   if (board[i][j].isMine) {
-      //       strHTML +=`${MINE}` ;
-      //   }
       strHTML += `\t</td>\n`;
     }
     strHTML += `</tr>\n`;
@@ -105,96 +74,28 @@ function renderBoard(board) {
   var elLife = document.querySelector(".life");
   elLife.innerText = ` lives left : ${gLife}`;
 }
-
-function getClassName(location) {
-  var cellClass = "cell-" + location.i + "-" + location.j;
-  return cellClass;
-}
-
-function getLocationByClassName(className) {
-  var num = className.match(/\d/g); // reg exp : g =  work on all matches.
-  num = num.join("");
-  var locationI = num[0];
-  var locationJ = num[1];
-  return { i: locationI, j: locationJ };
-}
-
-function setMinesNegsCount(board) {}
-
-function getEmptyCells() {
-  var empty = [];
-  for (var i = 1; i < gBoard.length - 1; i++) {
-    for (var j = 1; j < gBoard[0].length - 1; j++) {
-      var currCell = gBoard[i][j];
-      if (!currCell.gameElement) empty.push({ i, j });
-    }
-  }
-  return empty;
-}
-
-function countMineNegs(mat, pos) {
-  var positions = [];
-  var count = 0;
-  for (var i = pos.i - 1; i <= pos.i + 1; i++) {
-    if (i < 0 || i > mat.length - 1) continue;
-    for (var j = pos.j - 1; j <= pos.j + 1; j++) {
-      if (j < 0 || j > mat.length - 1) continue;
-      if (i === pos.i && j === pos.j) continue;
-      if (mat[i][j].isMine) {
-        count++;
-      } else {
-        if (mat[i][j].isFlag === true) continue;
-        positions.push({ i, j });
-      }
-    }
-  }
-  if (count === 0) {
-    for (var i = 0; i < positions.length; i++) {
-      neg(gBoard, positions[i]);
-      gBoard[positions[i].i][positions[i].j].isShown = false;
-      gGame.shownCount++;
-    }
-  }
-  renderCell(pos, count);
-  var statsShown = document.querySelector(".statsShown");
-  statsShown.innerText = `cells showen : ${gGame.shownCount} `;
-}
-function neg(mat, positions) {
-  var count = 0;
-  for (var i = positions.i - 1; i <= positions.i + 1; i++) {
-    if (i < 0 || i > mat.length - 1) continue;
-    for (var j = positions.j - 1; j <= positions.j + 1; j++) {
-      if (j < 0 || j > mat.length - 1) continue;
-      if (mat[i][j].isMine) {
-        count++;
-      }
-    }
-  }
-  renderCell(positions, count);
-}
-
+// responsable to every response to click event
 function cellClicked(posI, posJ) {
   if (gIsFirstMove) {
     for (var i = 0; i < gLevel.MINES; i++) {
-      gBoard[drawNum().i][drawNum().j].isMine = true;
+      var location = drawNum();
+      gBoard[location.i][location.j].isMine = true;
     }
     startTimer();
-     var dateObjbegin = new Date();
-     var timeCounter = dateObjbegin.getTime();
-
-    // gBoard[drawNum().i][drawNum().j].isMine = true;
-    //     gBoard[drawNum().i][drawNum().j].isMine = true;
+    var dateObjbegin = new Date();
+    var timeCounter = dateObjbegin.getTime();
     gIsFirstMove = false;
   }
-  gBoard[posI][posJ].isShown = false;
+  if (gBoard[posI][posJ].isShown === false) return ;
   if (gBoard[posI][posJ].isFlag) return;
+  if (gLastPos[0] === posI && gLastPos[1] === posJ) return;
+  else if (gBoard[posI][posJ].isShown === true) gBoard[posI][posJ].isShown = false;
   var statsShown = document.querySelector(".statsShown");
   statsShown.innerText = `cells showen : ${gGame.shownCount} `;
   if (!gBoard[posI][posJ].isMine) {
-    if (gLastPos[0] === posI && gLastPos[1] === posJ) return;
     gGame.shownCount++;
-    countMineNegs(gBoard, { i: posI, j: posJ });
-  } else {
+    cheakNeighbors(gBoard, { i: posI, j: posJ });
+  } else if (gBoard[posI][posJ].isMine) {
     gLife--;
     var elLife = document.querySelector(".life");
     elLife.innerText = ` lives left : ${gLife}`;
@@ -223,26 +124,55 @@ function cellClicked(posI, posJ) {
   cheakVictory();
   gLastPos = [posI, posJ];
 }
-
-function drawNum() {
-  var idxI = getRandomInt(0, gLevel.SIZE - 1);
-  var idxJ = getRandomInt(0, gLevel.SIZE - 1);
-  var num = { i: idxI, j: idxJ };
-  return num;
+// responsable to cheak direct neighbors
+function cheakNeighbors(mat, pos) {
+  var positions = [];
+  var count = 0;
+  for (var i = pos.i - 1; i <= pos.i + 1; i++) {
+    if (i < 0 || i > mat.length - 1) continue;
+    for (var j = pos.j - 1; j <= pos.j + 1; j++) {
+      if (j < 0 || j > mat.length - 1) continue;
+      if (i === pos.i && j === pos.j) continue;
+      if (mat[i][j].isMine) {
+        count++;
+      } else {
+        if (mat[i][j].isFlag === true) continue;
+        positions.push({ i, j });
+      }
+    }
+  }
+  if (count === 0) {
+    for (var i = 0; i < positions.length; i++) {
+      cheakNeighborsOfNeighbors(gBoard, positions[i]);
+      gBoard[positions[i].i][positions[i].j].isShown = false;
+      gGame.shownCount++;
+    }
+  }
+  renderCell(pos, count);
+  var statsShown = document.querySelector(".statsShown");
+  statsShown.innerText = `cells showen : ${gGame.shownCount} `;
 }
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+// responsable to cheak  neighbors of neighbors
+function cheakNeighborsOfNeighbors(mat, positions) {
+  var count = 0;
+  for (var i = positions.i - 1; i <= positions.i + 1; i++) {
+    if (i < 0 || i > mat.length - 1) continue;
+    for (var j = positions.j - 1; j <= positions.j + 1; j++) {
+      if (j < 0 || j > mat.length - 1) continue;
+      if (mat[i][j].isMine) {
+        count++;
+      }
+    }
+  }
+  renderCell(positions, count);
 }
-
+//render specific cell
 function renderCell(location, value) {
   var cellSelector = "." + getClassName(location);
   var elCell = document.querySelector(cellSelector);
   elCell.innerHTML = value;
 }
-
+//game over
 function gameOver() {
   var elGameStat = document.querySelector(".gameStat");
   elGameStat.innerText = "game over !";
@@ -252,7 +182,7 @@ function gameOver() {
   clearInterval(gInterval);
   gInterval = null;
 }
-
+// responsable on right click event
 function flag(event) {
   var pos = event.target;
   getLocationByClassName(pos.className);
@@ -260,6 +190,7 @@ function flag(event) {
     gBoard[getLocationByClassName(pos.className).i][
       getLocationByClassName(pos.className).j
     ];
+   if (!isCellFlag.isShown) return;
   if (isCellFlag.isFlag) {
     pos.innerText = "";
     isCellFlag.isFlag = false;
@@ -268,7 +199,6 @@ function flag(event) {
       gTrueFlages--;
     }
   } else if (!isCellFlag.isFlag) {
-    if (!isCellFlag.isShown) return;
     pos.innerText = FLAG;
     isCellFlag.isFlag = true;
     gGame.markedCount++;
@@ -281,43 +211,44 @@ function flag(event) {
   var statsMarked = document.querySelector(".statsMarked");
   statsMarked.innerText = `cells marked :  ${gGame.markedCount}`;
 }
-
+// responsable to cheak win
 function cheakVictory() {
   var numOfCells = gLevel.SIZE ** 2;
   if (numOfCells - gGame.shownCount - gTrueFlages === 0) {
- 
-stopTimer();
-const dateObjbeginFliped = new Date();
-timeCounterNow = dateObjbeginFliped.getTime();
-timePassed = parseInt((timeCounterNow - timeCounterFirstFlip) / 1000);
-if (timePassed > bestTime) {
-    if (firstWin === 0) {
-      bestTime = timePassed;
-      firstWin++;
-    }
-  } else {
-    bestTime = timePassed;
-  }
-  localStorage.setItem("currante time", timePassed);
-  localStorage.setItem("best time", bestTime);
-  time = localStorage.getItem("currante time");
-  winingTime = localStorage.getItem("best time");
-  
-  yourScore.innerHTML = `your score is: ${time}`;
-  bestScore.innerHTML = `the best time is : ${winingTime}`;
-  if (winingTime === time) {
-    // alert("you have reached the bset time ! well done ! ");
-  }
+    stopTimer();
 
+    // work in progress on bounos
+    // const dateObjbeginFliped = new Date();
+    // timeCounterNow = dateObjbeginFliped.getTime();
+    // timePassed = parseInt((timeCounterNow - timeCounterFirstFlip) / 1000);
+    // if (timePassed > bestTime) {
+    //   if (firstWin === 0) {
+    //     bestTime = timePassed;
+    //     firstWin++;
+    //   }
+    // } else {
+    //   bestTime = timePassed;
+    // }
+    // localStorage.setItem("currante time", timePassed);
+    // localStorage.setItem("best time", bestTime);
+    // time = localStorage.getItem("currante time");
+    // winingTime = localStorage.getItem("best time");
+
+    // yourScore.innerHTML = `your score is: ${time}`;
+    // bestScore.innerHTML = `the best time is : ${winingTime}`;
+    // if (winingTime === time) {
+    //   // alert("you have reached the bset time ! well done ! ");
+    // }
 
     document.querySelector(".win").innerText = "😎";
-    document.querySelector(".gameStat").innerText='you won ! well done !';
+    document.querySelector(".gameStat").innerText = "you won ! well done !";
     resetTimer();
     setTimeout(init, 2000);
     clearInterval(gInterval);
     gInterval = null;
   }
 }
+// choose game level by user
 function gameLevel() {
   var level = document.querySelector(".level").value;
 
--- js/utilites.js
+++ js/utilites.js
@@ -1,4 +1,3 @@
-// create an empty matrix
 function createMat(length) {
     var mat = [];
     for (var i = 0; i < length; i++) {
@@ -9,3 +8,30 @@ function createMat(length) {
     }
     return mat;
   }
+   // return class by location
+  function getClassName(location) {
+    var cellClass = "cell-" + location.i + "-" + location.j;
+    return cellClass;
+  }
+   // return location by class
+  function getLocationByClassName(className) {
+    var num = className.match(/\d/g); // reg exp : g =  work on all matches.
+    num = num.join("");
+    var locationI = num[0];
+    var locationJ = num[1];
+    return { i: locationI, j: locationJ };
+  }
+  // returne object with random idxs
+  function drawNum() {
+    var idxI = getRandomInt(0, gLevel.SIZE - 1);
+    var idxJ = getRandomInt(0, gLevel.SIZE - 1);
+    var num = { i: idxI, j: idxJ };
+    return num;
+  }
+  // get random number
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+  }
+  
